85387ce180a0a6b16dc7e477302db1366f693c71
mingchao.xu <271688304@qq.com>
2021-03-03 14:35:28 +0800

    Update dialogInstance.js
    修改弹窗组件

--- src/components/Dialog/dialogInstance.js
+++ src/components/Dialog/dialogInstance.js
@@ -3,49 +3,50 @@ import { Dialog } from 'element-ui'
 
 Dialog.newInstance = properties => {
   const _props = properties || {}
-  // 实例化一个组件，然后挂载到body上
   const Instance = new Vue({
-    components: { },
-    data() {
-      return {
-        show: false,
-        component: '',
-        data: {},
-        isOk: false
-      }
-    },
-
+    name:"c-modal",
+    componentName:"CModal",
+    data:Object.assign({},{
+      show:false,
+      component:"",
+      data:{},
+      isOk:false,
+      cptName:"data-form",
+      width:"600px",
+      footerHide:false,
+      okText:"确定",
+      canneText:"取消"
+    },_props),
     methods: {
-
+      cancelHandler() {
+        this.show = false
+        this.cancel && this.cancel()
+        this.remove()
+      },
       okHandler() {
-        if (typeof _props.beforCloseDialog === 'function') {
-          _props.beforCloseDialog(this, this.getFormInstance(this.$children), this.closeDialog)
-        } else {
-          this.closeDialog()
+        if(this.beforeCloseModal && typeof this.beforeCloseModal==='function'){
+          this.beforeCloseModal(this,this.getInstance(this.$children),this.closeModal)
+        }else{
+          this.closeModal()
         }
       },
-      closeDialog() {
-        _props.ok && _props.ok()
-        this.isOk = true
-        this.show = false
-        this.remove()
-      },
-      closeHandler() {
-        this.show = false
-        _props.cannel && _props.cannel()
-        this.remove()
-      },
-      getFormInstance(elementArr) {
-        let $form = null
-        elementArr.forEach(element => {
-          if (element && element.$options.name === 'data-form') {
-            $form = element
-          } else {
-            const form = this.getFormInstance(element.$children)
-            if (form) $form = form
+      getInstance(elArray){
+        let $component = null;
+        elArray.forEach(el=>{
+          if(el && el.$options.name===this.cptName){
+            $component = el
+          }else{
+            let component = this.getInstance(el.$children)
+            if(component) $component = component
           }
         })
-        return $form
+        return $component
+      },
+      closeModal(){
+        this.ok && this.ok()
+        this.isOk = true;
+        this.show = false;
+        this.remove()
       },
       remove() {
         setTimeout(() => {
@@ -53,71 +54,64 @@ Dialog.newInstance = properties => {
         }, 300)
       },
       destroy() {
-        if (document.body.contains(this.$el)) {
+        if(document.body.contains(this.$el)){
           document.body.removeChild(this.$el)
         }
         this.$destroy()
       },
-      onRemove() {}
     },
     render(h) {
       return h('el-dialog', {
-        staticClass: 'x-dialog',
         props: {
           ..._props,
           visible: this.show
         },
 
         on: {
-          close: () => {
-            if (this.isOk) return
-            this.closeHandler()
+          colse:()=>{
+            if(this.isOk) return;
+            this.cancelHander()
           }
 
         },
-        scopedSlots: {
-          default: () => {
-            let dialogContent = ''
-            if (_props.content) {
-              dialogContent = _props.content
-            } else if (_props.component) {
-              dialogContent = h(this.component, { staticClass: 'x-dialog-content', props: this.data }, [])
-            }
-            const footer = _props.footerHide ? '' : h('div', { staticClass: 'el-dialog__footer' }, [
-              <el-button onClick={this.closeHandler}>取消</el-button>,
-              <el-button type='primary' onClick={this.okHandler}>取消</el-button>
+        scopedSlots:{
+          default:()=>{
+            let modalContent = this.component ? h(this.component,{staticClass:"c-modal-content",props:this.data}) : this.content
+            let footer = this.footerHide ? "" : h("div",{staticClass:"el-dialog__footer"},[
+              <el-button onClick={this.cancelHandler}>{this.canneText}</el-button>,
+              <el-button type="primary" onClick={this.okHandler}>{this.okText}</el-button>
+
             ])
-            return h('div', { staticClass: 'c-dialog-body' }, [dialogContent, footer])
+            return h("div",{staticClass:"c-modal-body"},[modalContent,footer])
           }
         }
-      }, [])
+      })
     }
 
   })
   const component = Instance.$mount()
   document.body.appendChild(component.$el)
   // 通过闭包维护alert组件的引用
-  const dialog = Instance
+  const modal = Instance
   return {
     show(props) {
-      dialog.onRemove = props.onRemove
-      dialog.show = true
+      modal.onRemove = props.onRemove
+      modal.show = true
 
       if ('component' in props) {
-        dialog.component = props.component
+        modal.component = props.component
       }
 
       if ('data' in props) {
-        dialog.data = props.data
+        modal.data = props.data
       }
-      console.log(dialog)
-      return dialog
+      console.log(modal)
+      return modal
     },
     remove() {
-      dialog.show = false
-      dialog.remove()
-    },
-    component: dialog
+      modal.show = false
+      modal.remove()
+    }
   }
 }
 
